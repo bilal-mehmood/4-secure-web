2638a958c87c464608e4a27f18aa8225881f7bdb
Bilal Mehmood <bilalmehmood246@gmail.com>
2023-03-13 17:28:10 +0500

    Updated sendmail

--- sendemail.php
+++ sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "youremail@emailservice.com" );
+define( "RECIPIENT_NAME", "Bilal Mehmood" );
+define( "RECIPIENT_EMAIL", "bilalmehmood246@gmail.com" );
 
 
 // Read the form values
